cef6255feff0b66b250c68056ea430908c7ba2ae
Michael Wilkerson <mswilke1991@gmail.com>
2026-03-04 22:49:50 -0600

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -14,11 +14,6 @@ from helpers import apology, login_required, execute_read_query, execute_write_q
 # Configure application
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    # Access the variable Render already provides
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -353,3 +348,8 @@ def delete_account_from_search():
     else:
 
         return redirect("/account")
+
+if __name__ == "__main__":
+    # Access the variable Render already provides
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
